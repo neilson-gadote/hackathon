18cc93a2a827f33625860641e6555ce4ff42a4d4
neilson-gadote <59440136+neilson-gadote@users.noreply.github.com>
2021-10-24 01:41:10 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,38 +1,46 @@
 import random
 import base64
+import time
+
+def html_base(fundo, cont, img):
+    return """<html>
+                  <head>
+                      <title>GF - Comendador Ermelino/L12</title>
+                      <meta http-equiv='refresh' content='8'>
+                      <link rel='stylesheet' type='text/css' href='style.css'>
+                  </head>
+                  <body>
+                      <div class='container'><img src='data:image/jpeg;base64,""" + fundo + """'></div>
+                      <div class='centered'>Passageiros na plataforma:</br>"""+ cont +"""</div>
+                      <img src='data:image/jpeg;base64,""" + img + """'>
+                  </body>
+              </html>"""
 
 entrada = open("dados.txt").read()
 saida = open("sso.html", "w")
 
 cont = entrada.count('"}')
+i=int(0)
 
-cont = random.randint(-100, 100) + cont
-
-if cont < 100 :
-    data_uri = base64.b64encode(open('1.jpg', 'rb').read()).decode('utf-8')
-    saida.write("""<html>
-               <head><title>Teste</title></head>
-               <body>
-                <div style='float:left'>Quantidade de usuários na plataforma:"""+ str(cont) +""".</div>
-                <img src='data:image/jpeg;base64,""" + data_uri + """'>
-               </body>
-               </html>""")
-elif cont > 200 :
-    data_uri = base64.b64encode(open('3.jpg', 'rb').read()).decode('utf-8')
-    saida.write("""<html>
-               <head><title>Teste</title></head>
-               <body>
-                <div style='float:left'>Quantidade de usuários na plataforma:"""+ str(cont) +""".</div>
-                <img src='data:image/jpeg;base64,""" + data_uri + """'>
-               </body>
-               </html>""")
-else :
-    data_uri = base64.b64encode(open('2.jpg', 'rb').read()).decode('utf-8')
-    saida.write("""<html>
-               <head><title>Teste</title></head>
-               <body>
-                <div style='float:left'>Quantidade de usuários na plataforma:"""+ str(cont) +""".</div>
-                <img src='data:image/jpeg;base64,""" + data_uri + """'>
-               </body>
-               </html>""")
-
+while i < 10:
+    cont = random.randint(0, 400)   
+    saida.truncate(0)
+    if cont < 100:
+        fundo = base64.b64encode(open('fundo.jpg', 'rb').read()).decode('utf-8')
+        img = base64.b64encode(open('1.jpg', 'rb').read()).decode('utf-8')
+        saida.write(html_base(fundo, str(cont), img))
+    elif cont >= 100 and cont < 200:
+        fundo = base64.b64encode(open('fundo.jpg', 'rb').read()).decode('utf-8')
+        img = base64.b64encode(open('3.jpg', 'rb').read()).decode('utf-8')
+        saida.write(html_base(fundo, str(cont), img))
+    elif cont >= 200 and cont < 300:
+        fundo = base64.b64encode(open('fundo.jpg', 'rb').read()).decode('utf-8')
+        img = base64.b64encode(open('3.jpg', 'rb').read()).decode('utf-8')
+        saida.write(html_base(fundo, str(cont), img))
+    else:
+        fundo = base64.b64encode(open('fundo.jpg', 'rb').read()).decode('utf-8')
+        img = base64.b64encode(open('2.jpg', 'rb').read()).decode('utf-8')
+        saida.write(html_base(fundo, str(cont), img))
+             
+    time.sleep(8)
+    i += 1
